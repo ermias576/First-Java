b6f17ee93bce1b492185a83efd6588724d717cc3
ermias_tegegn <ermiastegegn576@gmail.com>
2022-06-27 14:43:31 +0300

    add method

--- Hello.java
+++ Hello.java
@@ -5,10 +5,10 @@ import java.util.Date;
  */
 public class Hello {
 
-    public static void main(String[] args) {
-        int age = 24;
-        // Date today = new Date();
-        // String msg = "Hey Java";
-        System.out.println(age);
-    }
+    // public static void main(String[] args) {
+    // int age = 24;
+    // Date today = new Date();
+    // String msg = "Hey Java";
+    // System.out.println(age);
+    // }
 }
---
+++ Method.java
@@ -0,0 +1,10 @@
+public class Method {
+
+    public static void main(String[] args) {
+        AddExclamation("Hey there");
+    }
+
+    public static void AddExclamation(String s) {
+        System.out.println(s + "!");
+    }
+}
