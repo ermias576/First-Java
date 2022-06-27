00053525b5ade0bbafd2bde82253990cd9fbc41d
ermias_tegegn <ermiastegegn576@gmail.com>
2022-06-27 23:24:01 +0300

    calling method from other class

--- Method.java
+++ Method.java
@@ -1,10 +1,22 @@
 public class Method {
 
     public static void main(String[] args) {
-        AddExclamation("Hey there");
+        // AddExclamation("Hey there");
+        /*
+         * String exclaiam = AddExclamation("Hey there");
+         * System.out.println(exclaiam);
+         */
+        Person a = new Person();
+        String person = a.IamPerson();
+        System.out.println(person);
     }
 
-    public static void AddExclamation(String s) {
-        System.out.println(s + "!");
+    /*
+     * public static void AddExclamation(String s) {
+     * System.out.println(s + "!");
+     * }
+     */
+    public static String AddExclamation(String s) {
+        return s + "!";
     }
 }
---
+++ Person.java
@@ -0,0 +1,5 @@
+public class Person {
+    public static String IamPerson() {
+        return "I am a Person";
+    }
+}
